40ebfdaca35594b161cf90c45781d75b939536b1
Venkat Kumar <isvenkatkumar@gmail.com>
2022-10-06 02:17:43 +0000

    First IDE

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from blog.models import Post, Tag
+# Register your models here.
+
+admin.site.register(Tag)
+
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields = {'slug': ('title',)}
+  list_display = ('title','published_at')
+
+admin.site.register(Post,PostAdmin)
