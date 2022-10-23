cf56438dd8fab30d773c98d7e8dd01f9e7d550a8
Venkat Kumar <isvenkatkumar@gmail.com>
2022-10-23 03:21:19 +0000

    Hashing

--- blango/settings.py
+++ blango/settings.py
@@ -136,6 +136,12 @@ class Dev(Configuration):
         "level": "DEBUG",
     },
 }
+  PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+  ]
 
       
   # Internationalization
