b7592124166254fed3492ded0e9267897c2ae49b
Venkat Kumar <isvenkatkumar@gmail.com>
2022-10-23 01:50:31 +0000

    Logging

--- blango/settings.py
+++ blango/settings.py
@@ -115,7 +115,29 @@ class Dev(Configuration):
       },
   ]
 
+  LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
 
+      
   # Internationalization
   # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render,get_object_or_404,redirect
 from blog.models import Post, Comment, Tag
 from django.utils import timezone
 from blog.forms import CommentForm
+import logging
+logger = logging.getLogger(__name__)
 # Create your views here.
 
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
  # print('\n\nPOST = {}\n\n'.format(posts))
+  logger.debug('Got %d posts',len(posts))
   return render(request,'blog/index.html',{'posts':posts})
 
 def post_detail(request,slug):
@@ -19,6 +22,7 @@ def post_detail(request,slug):
               comment.content_object = post
               comment.creator = request.user
               comment.save()
+              logger.info('Created comment on Post %d for user %s',post.pk,request.user)
               return redirect(request.path_info)
       else:
           comment_form = CommentForm()
