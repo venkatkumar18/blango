2b2aedf9565cdfb3573234e67c020dea68d31d44
Venkat Kumar <isvenkatkumar@gmail.com>
2022-11-05 15:48:38 +0000

    Oauth google

--- blango/settings.py
+++ blango/settings.py
@@ -46,11 +46,19 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      'django.contrib.sites',
       'django.contrib.staticfiles',
       'blog',
       'crispy_forms',
       'debug_toolbar',
-      'blango_auth'
+      'blango_auth',
+      'allauth',
+      'allauth.account',
+      'allauth.socialaccount',
+      'allauth.socialaccount.providers.google',
+
+
+
   ]
 
   MIDDLEWARE = [
@@ -173,7 +181,12 @@ class Dev(Configuration):
   # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
   DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+  SITE_ID = 1
 
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
   EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 class Prod(Dev):
   DEBUG = False
